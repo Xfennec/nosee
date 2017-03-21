d1c7271a7146aa7d0472fc8e01154fa27efb3742
Xfennec <xfennec@cqfd-corp.org>
2017-03-21 16:03:32 +0100

    adding two more script and probe samples (md-raid and df)

---
+++ etc/scripts/probes/mdstat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mdstat="/proc/mdstat"
+
+if [ ! -f "$mdstat" ]; then
+    (>&2 echo "ERROR: cant find md RAID support ($mdstat)")
+    exit 1
+fi
+
+fcount=$(grep -c "\[.*_.*\]" $mdstat)
+
+echo "MDRAID_ERR_ARRAYS:" $fcount
