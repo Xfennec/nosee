4438e5ee0e7cd17c591eca69a5a30fbbc242d832
Xfennec <xfennec@cqfd-corp.org>
2017-05-23 11:23:48 +0200

    memory probe results are now easier to understand + fix for the probe error condition

--- etc/scripts/probes/mem.sh
+++ etc/scripts/probes/mem.sh
@@ -15,13 +15,15 @@ if [ -z "$ma_supported" ]; then
 
     mem_total_mb=$(echo $mem | cut -d\  -f2)
     mem_free_mb=$(echo $mem | cut -d\  -f4)
+
     mem_cached_mb=$(echo $mem | cut -d\  -f7)
     mem_buffers_mb=$(echo $mem | cut -d\  -f6)
-
-    mem_used_ratio=$(echo "$mem" | awk '{printf("%.2f\n", $3/$2);}')
     mem_buffcache_mb=$(($mem_cached_mb + $mem_buffers_mb))
-    mem_allocated_mb=$(($mem_total_mb - $mem_free_mb - $mem_buffcache_mb))
-    mem_available=$(($mem_total_mb - $mem_allocated_mb))
+
+    mem_hardused_mb=$(echo "$mem" | awk '{printf("%.2f\n", $3-$5-$6-$7);}')
+    mem_hardused_ratio=$(echo $mem_hardused_mb $mem_total_mb | awk '{printf("%.2f", $1/$2);}')
+
+    mem_available_mb=$(($mem_free_mb + $mem_buffcache_mb))
 
     swap_total_mb=$(echo $swap | cut -d\  -f2)
     swap_free_mb=$(echo $swap | cut -d\  -f4)
@@ -29,10 +31,9 @@ if [ -z "$ma_supported" ]; then
     swap_used_ratio=$(echo "$swap" | awk '{printf("%.2f\n", $3/$2);}')
 else
     mem_total_mb=$(meminfo_fmt MemTotal)
-    mem_free_mb=$(meminfo_fmt MemFree)
-    mem_used_mb=$(( $mem_total_mb - $mem_free_mb ))
-    mem_available=$(meminfo_fmt MemAvailable)
-    mem_used_ratio=$(echo $mem_used_mb $mem_total_mb | awk '{printf("%.2f", $1/$2);}')
+    mem_available_mb=$(meminfo_fmt MemAvailable)
+    mem_hardused_mb=$(( $mem_total_mb - $mem_available_mb ))
+    mem_hardused_ratio=$(echo $mem_hardused_mb $mem_total_mb | awk '{printf("%.2f", $1/$2);}')
     mem_buffers_mb=$(meminfo_fmt Buffers)
     mem_cached_mb=$(meminfo_fmt Cached)
 
@@ -45,13 +46,14 @@ fi
 mem_buffcache_mb=$(($mem_cached_mb + $mem_buffers_mb))
 mem_buffcache_ratio=$(echo $mem_total_mb $mem_buffcache_mb\
     | awk '{printf("%.2f\n", $2/$1);}')
-
+mem_available_ratio=$(echo $mem_total_mb $mem_available_mb\
+    | awk '{printf("%.2f\n", $2/$1);}')
 
 echo "MEM_TOTAL_MB:" $mem_total_mb
-#sleep 10
-echo "MEM_FREE_MB:" $mem_free_mb
-echo "MEM_AVAILABLE_MB:" $mem_available
-echo "MEM_USED_RATIO:" $mem_used_ratio
+echo "MEM_AVAILABLE_MB:" $mem_available_mb
+echo "MEM_AVAILABLE_RATIO:" $mem_available_ratio
+echo "MEM_HARDUSED:" $mem_hardused_mb
+echo "MEM_HARDUSED_RATIO:" $mem_hardused_ratio
 echo "MEM_BUFFCACHE_MB:" $mem_buffcache_mb
 echo "MEM_BUFFCACHE_RATIO:" $mem_buffcache_ratio
 echo "SWAP_TOTAL_MB:" $swap_total_mb
