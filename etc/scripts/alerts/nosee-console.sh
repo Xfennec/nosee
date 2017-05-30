a572d6b915407d888d0fdc11b283a5df0e32ffd1
Xfennec <xfennec@cqfd-corp.org>
2017-05-30 17:34:02 +0200

    updating usage message

--- etc/scripts/alerts/nosee-console.sh
+++ etc/scripts/alerts/nosee-console.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    (>&2 echo "ERROR: give nosee console URL")
+    (>&2 echo "ERROR: give nosee console URL (ex: http://localhost:8080/alerts)")
     exit 1
 fi
 
